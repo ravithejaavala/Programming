15ba9f353beff48af91bad3ff3698f51626bcc41
“Ravitheja <ravithejareddy8008@gmail.com>
2025-03-22 19:40:34 +0530

    commit on 22-03

---
+++ Python/Python Classes/29 class 22-03.py
@@ -0,0 +1,108 @@
+'''
+
+IV -- IM  --- self  -- Individual
+
+CV -- CM -- cls  -- shareable & modifiable
+
+Class Variable and Class Method:
+-------------------------------
+A variable that is shared by all the instance of a class. The class variables are defined
+within a class but outside any of the class methods.
+the class variables are not used as frequently as instance variable
+
+**  the class variables are declared when a class is being constructed and they are not
+defined inside of any method of a class..so the scope of the class variables are entire
+class (we can able to access inside instance method and class method)
+
+Note:
+----
+the implict(first parameter) of the class method is *cls*
+while class method we need to call decorator (@classmethod)
+
+'''
+
+class Emp(object):
+
+    office_location = 'blr' # class variable
+    def _init_(self, name, age, sal):
+        self.name = name
+        self.age = age
+        self.sal = sal
+    def get_emp_details(self): # instance method
+        print(self.name, self.age, self.sal)
+
+    @classmethod
+    def get_emp_off(cls): # classmethod
+        print(cls.office_location)
+
+ravi = Emp('ravi',29, 345643)
+ravi.get_emp_details() # calling the instance method with the help of an object
+ravi.get_emp_off()
+
+hyma = Emp('hyma',24, 456789)
+hyma.get_emp_details() # calling the instance method with the help of an object
+hyma.get_emp_off()
+
+# way of calling the attributes
+print(ravi.office_location)
+print(ravi.name)
+
+print(Emp.office_location)
+# print(Emp.name)
+
+'''
+IV IM
+CV CM
+SV SM
+
+
+CV IM   --- 
+IV CM   --- let me know as an example it it works
+
+'''
+
+# CV -- IM
+
+class Student:
+
+    schol_name = 'xyz' # class variable
+
+    def _init_(self, name, roll, age):
+        self.name = name
+        self.roll = roll
+        self.age = age
+
+    def get_student(self):
+        print(self.name, self.roll, self.age, self.schol_name)
+
+bhanu = Student('bhanu',2345678, 23)
+bhanu.get_student()
+
+
+'''
+static variables and static method:
+---------------------------
+All objects share class or static variables. An instance or non-static variables are
+different for different objects(every object has a copy).
+
+static variables are defined inside the class definition, but outside of any method
+definitions. They are typically initialized with a value, just like an instance variable,
+but they can be accessed and modified through the class itself, rather than through the
+instance...
+
+
+Advtanges:
+-----
+Memory efficiency: static variables are allocated memory once when the object for the 
+class is created for the first time...
+
+clss scope : The static variables are created outside methods but inside of the class
+
+
+
+Note:
+----
+the static variables are defined inside the class definition
+we need to use the @static method, while writing static method which is mandatory...
+
+'''
--- Python/Python Practice/temp3.py
+++ Python/Python Practice/temp3.py
@@ -1,3 +1,4 @@
+"""
 def can_partition(stones):
     total_sum = sum(stones)
     
@@ -24,3 +25,217 @@ n = int(input("Enter the number of stones: "))
 stones = [int(input(f"Enter weight of stone {i + 1}: ")) for i in range(n)]
 
 print(can_partition(stones))
+
+
+
+class Vehicle:
+    wheels = 4  # Class attribute
+
+    @classmethod
+    def change_wheels(cls, new_wheels):
+        cls.wheels = new_wheels  # Modifying class attribute
+
+# Accessing class attribute
+print(Vehicle.wheels)  # Output: 4
+
+# Changing class attribute using cls
+Vehicle.change_wheels(6)
+print(Vehicle.wheels)  # Output: 6
+
+
+
+class Employee:
+    company = "TechCorp"
+
+    def __init__(self,name):
+        self.name = name
+    @classmethod
+    def set_company(cls, new_company):
+        cls.company = new_company
+    
+    def is_adult(age):
+        if age>=18:
+            return True
+        else:
+            return False
+class Manager(Employee):
+    def __init__(self, name,department):
+        self.department = department
+        super().__init__(name)
+    def print_details(self):
+        print("Manager name = ", self.name," Manager Department = ", self.department)
+
+m1=Manager("Alice", "HR")
+
+Manager.print_details(m1)
+
+
+print(m1.name)
+print(m1.department)
+print(m1.company)
+
+Manager.set_company("InnoTech")
+
+print(m1.company) 
+
+print(Manager.is_adult(20))
+print(Manager.is_adult(15))
+
+
+class Vehicle:
+    vehicle_type = "General"
+
+    def __init__(self, brand):
+        self.brand = brand
+    
+    @classmethod
+    def set_vehicle_type(cls, new_type):
+        cls.vehicle_type = new_type
+    
+    @staticmethod
+    def is_motorized(engine_type):
+        return engine_type == "petrol" or "diesel"
+
+class Car(Vehicle):
+    def __init__(self, brand, model):
+        super().__init__(brand)
+        self.model = model
+
+car1 = Car("Toyota", "Corolla")
+
+print(car1.brand)      
+print(car1.model)     
+print(car1.vehicle_type)
+
+Car.set_vehicle_type("Four-Wheeler")
+print(car1.vehicle_type)
+
+print(Car.is_motorized("petrol")) 
+print(Car.is_motorized("electric")) 
+
+
+
+class Employee:
+    company = "TechCorp"
+
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+    
+    @classmethod
+    def new_company(cls, new_company):
+        cls.company = new_company
+    
+    @staticmethod
+    def check_salary(salary):
+        return salary >= 50000
+    
+    def display_details(self):
+        print("Name : ", self.name," Salary : ", self.salary)
+
+    
+class Manager(Employee):
+    def __init__(self, name, salary, department):
+        super().__init__(name, salary)
+        self.department = department
+    
+    def display_details(self):
+        super().display_details()
+        print("Department:", self.department)
+
+
+class Developer(Employee):
+    def __init__(self, name, salary, programming_language):
+        super().__init__(name, salary)
+        self.programming_language = programming_language
+
+    def knows_language(self, lang):
+        return lang == self.programming_language
+    
+
+m1 = Manager("Alice", 70000, "HR")
+d1 = Developer("Bob", 60000, "Python")
+
+m1.display_details()
+d1.display_details()
+
+Employee.new_company("InnoTech")
+print(m1.company)
+print(d1.company)
+
+print(Employee.check_salary(m1.salary))
+print(Employee.check_salary(d1.salary))
+
+print(d1.knows_language("Java"))
+print(d1.knows_language("Python"))
+
+
+
+class Employee:
+    company = "TechCorp"
+
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+    
+    @classmethod
+    def new_company(cls, new_company):
+        cls.company = new_company
+    
+    @staticmethod
+    def check_salary(salary):
+        return salary >= 50000
+    
+    def display_details(self):
+        print("Name : ", self.name," Salary : ", self.salary)
+
+    
+class Manager(Employee):
+    def __init__(self, name, salary, department):
+        super().__init__(name, salary)
+        self.department = department
+    
+    def display_details(self):
+        super().display_details()
+        print("Department:", self.department)
+
+
+class Developer(Employee):
+    def __init__(self, name, salary, programming_language):
+        super().__init__(name, salary)
+        self.programming_language = programming_language
+
+    def knows_language(self, lang):
+        return lang == self.programming_language
+    
+
+m1 = Manager("Alice", 70000, "HR")
+d1 = Developer("Bob", 60000, "Python")
+
+m1.display_details()
+d1.display_details()
+
+
+"""
+
+
+class Employee:
+    company = "TechCorp"
+
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+    
+    @classmethod
+    def new_company(cls, new_company):
+        cls.company = new_company
+    
+    def display_details(self):
+        print("Name : ", self.name," Salary : ", self.salary," Company:",self.company)
+
+# CV IM
+# IV CM
+
+m1 = Employee("Alice", 70000)
+
+m1.display_details()
